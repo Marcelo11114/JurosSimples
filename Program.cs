69e63b792fe2710cba436637f06ffbf587bac93d
Marcelo11114 <dedosearte@hotmail.com>
2020-09-23 17:18:16 -0300

    Juros Simples

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace JurosSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double capital,taxa,juros,total,mes;
+            
+
+            Console.Clear();
+            Console.WriteLine("+-------------+\n");
+            Console.WriteLine("|JUROS SIMPLES|\n");
+            Console.WriteLine("+-------------+\n");
+            
+            Console.Write("Capital [c] (R$).......:");
+            capital=Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Taxa de juros [i] (%)..:"); 
+            taxa=Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Tempo [t] (meses)......:"); 
+            mes=Convert.ToDouble(Console.ReadLine());
+
+
+            juros=((capital/100)*taxa)*mes;
+            total = capital + juros;
+            Console.WriteLine($"\nJuros (R$).....: {juros:c}"); 
+            Console.Write($"Montante (R$)..: {total:c}"); 
+
+            Console.Write("\n\npronto?");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
